de3809f37720a2afffe0fe6eec19ed8aa94735b9
mohdamaan570 <mohdamaan570@gmail.com>
2022-10-05 19:47:39 +0530

    donebyamaan

--- backend/middleware/error.js
+++ backend/middleware/error.js
@@ -4,14 +4,14 @@ module.exports = (err,req,res,next)=>{
     err.statusCode = err.statusCode || 500;
     err.message = err.message || "Internal Server error";
 
-    //MonngoDB error
+    //wrong MonngoDB ID error
     if(err.name == "CastError"){
         const message = `Resource not Found, invalid ${err.path}`
         err = new ErrorHandler(message,400);
     }
 
     res.status(err.statusCode).json({
-        status: false,
-        error: err
+        success: false,
+        message: err.message
     })
 }
